cbc5835c8e7000b9e1fd4e21054d6c56234c1dee
shoaibbadshah <shoaibbadshah45@gmail.com>
2022-01-11 19:46:09 +0500

    buttons

--- pages/components/booking__page/Rooms.js
+++ pages/components/booking__page/Rooms.js
@@ -13,9 +13,29 @@ const Rooms = () => {
           className="rounded-lg"
         />
         <h1 className="font-bold text-2xl">Simple Room</h1>
-        <h1 className="font-semibold">
+        <h1 className="font-semibold mt-2">
           Lorem Ipsum is simply dummy text of the prinng and typeseng industry.
         </h1>
+
+        {/* price of the room */}
+        <div className="flex justify-between mr-28 mt-7">
+          <h1 className="font-semibold text-3xl ">
+            <span className="text-orange-300 ">Rs. 2,000</span>
+            /Night
+          </h1>
+          <div className="">
+            <input
+              type={"button"}
+              value={"See Details"}
+              className="border-2 rounded-lg font-semibold text-xl px-7 py-3 mr-4 text-orange-400 border-orange-400"
+            />
+            <input
+              type={"button"}
+              value={"Book Now"}
+              className="border-2 border-orange-400 rounded-lg font-semibold text-xl px-7 py-3 bg-orange-400 text-white"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
