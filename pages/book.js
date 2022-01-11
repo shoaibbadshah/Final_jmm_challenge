b8f91aa8bb82c49b4b74ea3a379e34701cc89325
shoaibbadshah <shoaibbadshah45@gmail.com>
2022-01-11 17:02:54 +0500

    form

--- pages/book.js
+++ pages/book.js
@@ -1,9 +1,16 @@
 import React from "react";
+import Form from "./components/booking__page/Form";
+import Rooms from "./components/booking__page/Rooms";
+import Navbar from "../pages/components/Navbar";
 
 const book = () => {
   return (
     <div>
-      <h1>hi this is </h1>
+      <Navbar />
+      <div className="flex justify-around">
+        <Form />
+        <Rooms />
+      </div>
     </div>
   );
 };
---
+++ pages/components/booking__page/Form.js
@@ -0,0 +1,80 @@
+import React from "react";
+
+const Form = () => {
+  return (
+    <div>
+      <form className="border-2 w-full px-5 py-8 bg-orange-100">
+        <label className="font-bold text-xl">Check In</label>
+        <br />
+        <input
+          type={"date"}
+          defaultValue="2020-06-01"
+          className="bg-white border-2 w-96 px-7 py-3 mb-4 mt-4 text-yellow-500"
+        />
+        <br />
+        <label className="font-bold text-xl">Check Out</label>
+        <br />
+        <input
+          type={"date"}
+          defaultValue="2022-01-21"
+          className="bg-white border-2 w-96 px-7 py-3 mt-4 mb-4 text-yellow-500"
+        />
+        <br />
+        <label className="font-bold text-xl" for="cars">
+          Rooms
+        </label>
+        <br />
+        <select
+          className="bg-white border-2 w-96 px-7 py-3 mt-4 text-yellow-500"
+          name="cars"
+          id="cars"
+        >
+          <option value="volvo">01</option>
+          <option value="saab">02</option>
+          <option value="mercedes">03</option>
+          <option value="audi">04</option>
+        </select>
+        <br />
+        <br />
+        <label className="font-bold text-xl" for="cars">
+          Adults
+        </label>
+        <br />
+        <select
+          className="bg-white border-2 w-96 px-7 py-3 mt-4 text-yellow-500"
+          name="cars"
+          id="cars"
+        >
+          <option value="volvo">01</option>
+          <option value="saab">02</option>
+          <option value="mercedes">03</option>
+          <option value="audi">04</option>
+        </select>
+        <br />
+        <br />
+        <label className="font-bold text-xl" for="cars">
+          Childrens
+        </label>
+        <br />
+        <select
+          className="bg-white border-2 w-96 px-7 py-3 mt-4 mb-4 text-yellow-500"
+          name="cars"
+          id="cars"
+        >
+          <option value="volvo">01</option>
+          <option value="saab">02</option>
+          <option value="mercedes">03</option>
+          <option value="audi">04</option>
+        </select>
+        <br />
+        <input
+          type={"button"}
+          value={"Check Rates"}
+          className="text-white border-2 rounded-lg font-bold w-96 px-7 py-3 mt-4 bg-orange-400"
+        />
+      </form>
+    </div>
+  );
+};
+
+export default Form;
